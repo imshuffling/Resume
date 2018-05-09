a3ca6fcdef9b34969e1f5e1670ad3f13a697125c
David Riches <hi@davidrich.es>
2018-05-09 16:19:24 +0100

    updating package.json

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var markdown = require('gulp-markdown');
-
-gulp.task('default', () =>
-    gulp.src('index.md')
-        .pipe(markdown())
-        .pipe(gulp.dest(''))
-);
