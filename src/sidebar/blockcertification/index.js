2c3b4c321c77d5d912163966debd9725d72b0d54
David Riches <hi@davidrich.es>
2023-05-22 20:25:40 +0100

    updating certs

--- src/sidebar/blockcertification/index.js
+++ src/sidebar/blockcertification/index.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import Contentful from '../../images/contentful.png';
-import Acquia from '../../images/acquia.png';
-import Bigcommerce from '../../images/bigcommerce.png';
+import React from "react";
+import Contentful from "../../images/contentful.png";
+import Acquia from "../../images/acquia.png";
+import Bigcommerce from "../../images/bigcommerce.png";
+import HeadlessCreator from "../../images/headlesscreator.png";
 
-export default function BlockCertification( {title, items} ) {
+export default function BlockCertification({ title, items }) {
   return (
     <section className="certification">
-        <h3 className="headline">{title}</h3>
-          {items.map((i, id) =>
-              <div className="certification__item" key={id}>
-                <div className="certification__icon">
-                  { i.type[0] === 'Contentful' && <img alt={'Contentful'} src={Contentful} /> }
-                  { i.type[0] === 'Acquia' && <img alt={'Acquia'} src={Acquia} /> }
-                  { i.type[0] === 'bigcommerce' && <img alt={'Bigcommerce'} src={Bigcommerce} /> }
-                </div>
-                <div className="certification__copy">
-                  <strong><a href={i.link}>{i.title}</a></strong>
-                  <div className="certification__year">{i.year}</div>
-                </div>
-              </div>
-          )}
+      <h3 className="headline">{title}</h3>
+      {items.map((i, id) => (
+        <div className="certification__item" key={id}>
+          <div className="certification__icon">
+            {i.type[0] === "Contentful" && (
+              <img alt={"Contentful"} src={Contentful} />
+            )}
+            {i.type[0] === "Acquia" && <img alt={"Acquia"} src={Acquia} />}
+            {i.type[0] === "bigcommerce" && (
+              <img alt={"Bigcommerce"} src={Bigcommerce} />
+            )}
+            {i.type[0] === "HeadlessCreator" && (
+              <img alt={"HeadlessCreator"} src={HeadlessCreator} />
+            )}
+          </div>
+          <div className="certification__copy">
+            <strong>
+              <a href={i.link}>{i.title}</a>
+            </strong>
+            <div className="certification__year">{i.year}</div>
+          </div>
+        </div>
+      ))}
     </section>
   );
 }
