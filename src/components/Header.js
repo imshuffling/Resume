94c2bd9dcbf4f2b25188fb8f541f4a762d8006c2
David Riches <hi@davidrich.es>
2024-09-27 16:27:31 +0100

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { FaMapMarkerAlt, FaPhoneAlt, FaLink, FaAt } from "react-icons/fa";
 import { GatsbyImage } from "gatsby-plugin-image";
+import { useMediaQuery } from "react-responsive";
 
 export default function Header(props) {
   const { title, introText, location, phone, email, website, image } =
     props.header;
+
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 959 });
 
   // Function to trigger print dialog
   const handlePrint = () => {
@@ -12,17 +15,17 @@ export default function Header(props) {
   };
 
   return (
-    <div className="flex flex-col-reverse md:grid md:grid-cols-[2fr_1fr] gap-4 md:gap-6 mb-4 md:mb-6">
+    <div className="mb-4 flex flex-col-reverse gap-4 md:mb-6 md:grid md:grid-cols-[2fr_1fr] md:gap-6">
       <div className="details_wrap flex flex-col gap-2 md:gap-6">
         <div>
-          <h1 className="text-lg md:text-3xl mb-0 text-davidblack font-bold uppercase">
+          <h1 className="mb-0 text-lg font-bold uppercase text-davidblack md:text-3xl">
             {title}
           </h1>
-          <h2 className="intro !mt-0 text-md md:text-2xl text-davidblue font-semibold">
+          <h2 className="intro !mt-0 text-md font-semibold text-davidblue md:text-2xl">
             {introText}
           </h2>
         </div>
-        <div className="grid grid-cols-2 gap-x-4 md:gap-x-6 gap-y-1 md:gap-y-2 details prose prose-sm md:prose-md lg:prose-lg xl:prose-xl ">
+        <div className="details prose prose-sm grid grid-cols-2 gap-x-4 gap-y-1 md:prose-md lg:prose-lg xl:prose-xl md:gap-x-6 md:gap-y-2">
           <div className="flex flex-row items-center gap-1">
             <FaMapMarkerAlt className="fill-davidpink" />
             {location}
@@ -41,7 +44,7 @@ export default function Header(props) {
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-2 mx-auto print:hidden">
+      <div className="mx-auto flex flex-col gap-2 print:hidden">
         <div className="image-wrap h-[160px] w-[160px] md:h-[230px] md:w-[230px]">
           <GatsbyImage
             className="image-default"
@@ -50,12 +53,14 @@ export default function Header(props) {
           />
         </div>
 
-        <button
-          onClick={handlePrint}
-          className="bg-davidpink hover:bg-davidDarkPink hover:no-underline underline text-white font-bold py-1 px-1 rounded"
-        >
-          Download as PDF
-        </button>
+        {!isTabletOrMobile && (
+          <button
+            onClick={handlePrint}
+            className="hover:bg-davidDarkPink rounded bg-davidpink px-1 py-1 font-bold text-white underline hover:no-underline"
+          >
+            Download as PDF
+          </button>
+        )}
       </div>
     </div>
   );
