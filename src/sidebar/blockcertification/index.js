9b846d18438a9213b3f7f9a1b93889eb7cc64fde
David Riches <hi@davidrich.es>
2025-05-18 07:37:44 +0100

    certs stuff

--- src/sidebar/blockcertification/index.js
+++ src/sidebar/blockcertification/index.js
@@ -3,10 +3,34 @@ import { FaAlgolia, FaLinkedin } from "react-icons/fa";
 import { SiBigcommerce, SiContentful, SiMiro } from "react-icons/si";
 
 import Acquia from "../../images/acquia.png";
+import ContentfulCert from "../../images/contentful-cert.png";
+import ContentfulManager from "../../images/contentful-manager-cert.png";
 import HeadlessCreator from "../../images/headlesscreator.png";
 
+const getContentfulIcon = (title) => {
+  if (title === "Contentful Certified Professional") {
+    return (
+      <img
+        className="!m-0 not-prose"
+        alt="Contentful Certified Professional"
+        src={ContentfulCert}
+      />
+    );
+  } else if (title === "Contentful Certified Content Manager") {
+    return (
+      <img
+        className="!m-0 not-prose"
+        alt="Contentful Certified Content Manager"
+        src={ContentfulManager}
+      />
+    );
+  } else {
+    return <SiContentful className="h-5 w-5" />;
+  }
+};
+
 const iconMap = {
-  Contentful: <SiContentful className="h-5 w-5" />,
+  Contentful: (title) => getContentfulIcon(title),
   Algolia: <FaAlgolia className="fill-[#0639DC] w-5 h-5" />,
   Acquia: <img className="!m-0 not-prose" alt="Acquia" src={Acquia} />,
   bigcommerce: <SiBigcommerce className="h-5 w-5" />,
@@ -21,9 +45,17 @@ const iconMap = {
   Miro: <SiMiro className="bg-[#FFD02F] p-1 rounded-sm w-5 h-5" />,
 };
 
-const CertificationIcon = ({ type }) => iconMap[type] || null;
+const CertificationIcon = ({ type, title }) => {
+  if (type === "Contentful") {
+    return iconMap[type](title);
+  }
+  return iconMap[type] || null;
+};
 
 export default function BlockCertification({ title, items }) {
+  // console.log("items", items);
+  // console.log("title", title);
+
   return (
     <article className="certification mb-2 md:mb-0 prose prose-sm order-4 md:prose-md lg:prose-md xl:prose-lg">
       <h3 className="headline border-b-2 text-lg">{title}</h3>
@@ -31,7 +63,7 @@ export default function BlockCertification({ title, items }) {
         {items.map((i, id) => (
           <div className="certification__item flex items-center gap-2" key={id}>
             <div className="certification__icon h-5 w-5">
-              <CertificationIcon type={i.type[0]} />
+              <CertificationIcon type={i.type[0]} title={i.title} />
             </div>
             <div className="certification__copy text-xs md:text-sm">
               <strong>
